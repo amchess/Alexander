73fc2b031b31c93b83e39156fd4cab242e7d05f5
amchess <amchess@libero.it>
2024-04-11 22:09:49 +0200

    Alexander 1.1 Nodestime option corrected futility pruning: parent node

--- src/misc.cpp
+++ src/misc.cpp
@@ -79,7 +79,7 @@ namespace Alexander {
 namespace {
 
 // Version number or dev.
-constexpr std::string_view version = "1.0";
+constexpr std::string_view version = "1.1";
 
 // Our fancy logging facility. The trick here is to replace cin.rdbuf() and
 // cout.rdbuf() with two Tie objects that tie cin and cout to a file stream. We
--- src/native.sh
+++ src/native.sh
@@ -1,4 +1,4 @@
 make profile-build ARCH=native COMP=gcc
 strip alexander
-mv alexander Alexander1.0-native
+mv alexander Alexander1.1-native
 make clean
