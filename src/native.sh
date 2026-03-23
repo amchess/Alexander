250d6db4075192219671a80ee7a2d4062166c795
amchess <amchess@libero.it>
2026-03-23 10:07:54 +0100

    Alexander 8.2

--- src/native.sh
+++ src/native.sh
@@ -2,5 +2,5 @@
 make clean
 make profile-build ARCH=native COMP=gcc ENV_CXXFLAGS="-DNNUE_EMBEDDING_OFF" -j$(nproc)
 strip alexander
-mv 'alexander' 'Alexander8.1-native'
+mv 'alexander' 'Alexander8.2-native'
 make clean
